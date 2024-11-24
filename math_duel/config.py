b0f6bf8ef2f70ed79c2f3d751fcd11a8f1a371c9
sunshiine3103 <vasileva.sveta.3.1.0.3@gmail.com>
2024-11-24 21:45:42 +0300

    Create config.py

---
+++ math_duel/config.py
@@ -0,0 +1,6 @@
+import random
+
+num1 = random.randint(-1000, 1000)
+num2 = random.randint(-1000, 1000)
+ops = ['+', '-', '*', ':']
+cur_op = random.choice(ops) #генерируем случайные числа и операции
